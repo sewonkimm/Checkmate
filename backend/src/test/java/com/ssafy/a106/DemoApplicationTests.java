4439ec138e6af8c9bb194464062e9501216153d1
이은택 <rudus1012@naver.com>
2021-04-17 16:01:55 +0900

    backend initial commit

---
+++ backend/src/test/java/com/ssafy/a106/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.a106;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
