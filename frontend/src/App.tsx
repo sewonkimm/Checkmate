cc13300a0586d92c0baa24b3650273def1418e97
hsy118 <hsy118@hotmail.co.kr>
2021-04-20 17:34:18 +0900

    ci: 🎡 Add: React-router & styled-components
    ✅ Closes: S04P31A106-151

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,6 +13,7 @@ const App: React.FC = () => {
   return (
     // ThemeProvide 가 theme.ts에서 지정한 테마
     // switch는 라우터 감싸는 태그로 리액트 라우터에서 사용
+    // exact는 /가 /로 시작하는 다른 경로와 겹치지 않기위해 사용
     <ThemeProvider theme={theme}>
       <Switch>
         <Route path="/" exact component={Home} />
