fc61ee63af593df1c8481f6dcadd4bca7af1713b
rudus1012 <rudus1012@naver.com>
2021-04-20 16:23:23 +0900

    feat: 🍎 회원가입 기능 구현
    - 비밀번호 암호화, 유효성 검사 후 회원가입
    
    ✅ Closes: #S04P31A106-119

---
+++ backend/src/main/java/com/ssafy/checkmate/member/repository/MemberRepository.java
@@ -0,0 +1,12 @@
+package com.ssafy.checkmate.member.repository;
+
+import com.ssafy.checkmate.member.dto.Member;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends CrudRepository<Member, String> {
+
+    public Member findMemberByMemberEmail(String memberEmail);
+
+}
