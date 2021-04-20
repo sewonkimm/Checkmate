5d1fb3057dbf46909c810e4ebfceaafee4cb728a
hsy118 <hsy118@hotmail.co.kr>
2021-04-20 16:54:51 +0900

    ci: 스타일 컴포넌트, 리액트 라우터

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
+import {GlobalStyle} from './styles/global-styles'
+
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
+    <GlobalStyle />
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root'),
 );
