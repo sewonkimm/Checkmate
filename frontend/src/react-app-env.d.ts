f38b0b9a4afe7a9bd49fba0e7ab880bdbb3bb0f9
sewonkimm <swon962@gmail.com>
2021-04-21 16:40:51 +0900

    docs: ✏️ 주석 추가
    - 페이지별로 폴더를 제작하는 방식으로 프로젝트 구조를 수정했습니다.
    - 각 파일마다 주석을 추가해주었습니다.

--- frontend/src/react-app-env.d.ts
+++ frontend/src/react-app-env.d.ts
@@ -1,1 +1,7 @@
+/* 
+react - app - env.d.ts 
+: create - react - app으로 시작된 프로젝트와 관련된 TypeScript 타입 선언을 참조합니다.
+  그리고 이러한 타입 선언은 bmp, gif, jpg, jpg, png, webp 및 svg와 같은 리소스 파일을 가져오는데 대한 지원을 추가합니다.
+*/
+
 /// <reference types="react-scripts" />
