2497c8e79df92bbddc5083de00704ab7d94851df
ingorithm <ingu94@gmail.com>
2021-04-21 15:17:49 +0900

    feat: 🍎 #S04P31A106-112 질문 등록
    ✅ Closes: #S04P31A106-112

---
+++ backend/src/main/java/com/ssafy/checkmate/question/repository/QuestionRepository.java
@@ -0,0 +1,9 @@
+package com.ssafy.checkmate.question.repository;
+
+import com.ssafy.checkmate.question.dto.Question;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuestionRepository extends CrudRepository<Question, String> {
+}
