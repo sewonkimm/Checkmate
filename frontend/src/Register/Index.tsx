d8c1be5005535430e5abfa201a03a0371ab8750b
sewonkimm <swon962@gmail.com>
2021-04-26 20:37:27 +0900

    refactor: 🍱 Register/index.tsx 리팩토링
    - prettier 적용
    - registerGroup 타입 설정
    - 조건부 렌더링 가독성 향상을 위해 삼항연산자에서 && 연산으로 수정
    
    ✅ Closes: #S04P31A106-184

--- frontend/src/Register/Index.tsx
+++ frontend/src/Register/Index.tsx
@@ -5,55 +5,39 @@ Register/Index.tsx
 
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import MotherLanguage from './MotherLanguage'
-import InputEmail from './InputEmail'
+import InputEmail from './InputEmail';
+import MotherLanguage from './MotherLanguage';
 import Nickname from './Nickname';
-import RegisterComplete from './RegisterComplete'
-
-import MotherLanguage from './MotherLanguage'
-import InputForm from './InputForm'
+import RegisterComplete from './RegisterComplete';
 
 const Register: React.FC = () => {
-  const [step, setStep] = useState<number>(0)
-  const [selectedLanguage, setSelectedLanguage] = useState<string>('')
-
-  // 컴포넌트가 4개가있다
-  // 다음버튼을 누르면 step ++, 그러면 첫번째 컴포넌트는 display:none이 되고 2번째 컴포넌트가 보여지게 된다.
-
-
+  const [step, setStep] = useState<number>(0); // 몇 번째 컴포넌트가 보여질지 정해주는 state
+  const [selectedLanguage, setSelectedLanguage] = useState<string>('');
 
   const handleNextBtn = () => {
-    if (step >= 0 && step < registerGroup.length-1) {
-      setStep(step + 1)
-    } else {
-      // eslint-disable-next-line no-alert
-      window.alert('out of index')
+    if (step >= 0 && step < registerGroup.length - 1) {
+      setStep(step + 1); // 다음 컴포넌트로
     }
-  }
+  };
   const handlePrevBtn = () => {
     if (step > 0 && step < registerGroup.length) {
-      setStep(step - 1)
-    } else {
-      // eslint-disable-next-line no-alert
-      window.alert('out of index')
+      setStep(step - 1); // 이전 컴포넌트로
     }
-  }
+  };
 
-  const setLang = (language:string) => {
-    // props로 전달받은 선택한 언어로 스테이트를 바꿈
-    setSelectedLanguage(language)
-  }
+  const setLang = (language: string) => {
+    // 선택한 언어로 language 값을 바꿈
+    setSelectedLanguage(language);
+  };
 
-  // 컴포넌트를 갖고 있는 배열 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const registerGroup: Array <any> = [
-    <MotherLanguage setLang={setLang} />, 
+  const registerGroup: Array<JSX.Element> = [
+    <MotherLanguage setLang={setLang} />,
     <InputEmail />,
     <Nickname />,
     <RegisterComplete />,
-  ]
+  ];
 
-  return(
+  return (
     <RegisterWrap>
       <div>
         {step + 1} / {registerGroup.length}
@@ -61,20 +45,14 @@ const Register: React.FC = () => {
       {selectedLanguage}
       {registerGroup[step]}
       <div>
-        {step > 0 ? 
-          (<PrevBtn onClick={handlePrevBtn}>이전으로</PrevBtn>) : 
-          (null)
-        }
+        {step > 0 && <PrevBtn onClick={handlePrevBtn}>이전으로</PrevBtn>}
         <NextBtn onClick={handleNextBtn}>다음으로</NextBtn>
       </div>
     </RegisterWrap>
-    
-  ) 
-    
-
+  );
 };
 
 const RegisterWrap = styled.div``;
 const NextBtn = styled.button``;
-const PrevBtn =styled.button``;
+const PrevBtn = styled.button``;
 export default Register;
