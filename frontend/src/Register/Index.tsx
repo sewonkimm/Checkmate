399dc8fa27e1dd73b4fd288ad49e6e0b93f7a689
hsy118 <hsy118@hotmail.co.kr>
2021-04-26 17:29:58 +0900

    feat: 🍎 회원가입 컴포넌트 연결 & 모국어 설정 부모 컴포넌트에게 props 추가
    ✅ Closes: #S04P31A106-184

--- frontend/src/Register/Index.tsx
+++ frontend/src/Register/Index.tsx
@@ -3,16 +3,75 @@ Register/Index.tsx
 : 회원가입 페이지
 */
 
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import MotherLanguage from './MotherLanguage'
+import InputEmail from './InputEmail'
+import Nickname from './Nickname';
+import RegisterComplete from './RegisterComplete'
 
 const Register: React.FC = () => {
-  return <Signup> 회원가입 페이지</Signup>;
+  const [step, setStep] = useState<number>(0)
+  const [selectedLanguage, setSelectedLanguage] = useState<string>('')
+
+  // 컴포넌트가 4개가있다
+  // 다음버튼을 누르면 step ++, 그러면 첫번째 컴포넌트는 display:none이 되고 2번째 컴포넌트가 보여지게 된다.
+
+
+
+  const handleNextBtn = () => {
+    if (step >= 0 && step < registerGroup.length-1) {
+      setStep(step + 1)
+    } else {
+      // eslint-disable-next-line no-alert
+      window.alert('out of index')
+    }
+  }
+  const handlePrevBtn = () => {
+    if (step > 0 && step < registerGroup.length) {
+      setStep(step - 1)
+    } else {
+      // eslint-disable-next-line no-alert
+      window.alert('out of index')
+    }
+  }
+
+  const setLang = (language:string) => {
+    // props로 전달받은 선택한 언어로 스테이트를 바꿈
+    setSelectedLanguage(language)
+  }
+
+  // 컴포넌트를 갖고 있는 배열 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const registerGroup: Array <any> = [
+    <MotherLanguage setLang={setLang} />, 
+    <InputEmail />,
+    <Nickname />,
+    <RegisterComplete />,
+  ]
+
+  return(
+    <RegisterWrap>
+      <div>
+        {step + 1} / {registerGroup.length}
+      </div>
+      {selectedLanguage}
+      {registerGroup[step]}
+      <div>
+        {step > 0 ? 
+          (<PrevBtn onClick={handlePrevBtn}>이전으로</PrevBtn>) : 
+          (null)
+        }
+        <NextBtn onClick={handleNextBtn}>다음으로</NextBtn>
+      </div>
+    </RegisterWrap>
+    
+  ) 
+    
+
 };
 
-const Signup = styled.div`
-  color: ${(props) => props.theme.color.main};
-  background-color: black;
-`;
-
+const RegisterWrap = styled.div``;
+const NextBtn = styled.button``;
+const PrevBtn =styled.button``;
 export default Register;
