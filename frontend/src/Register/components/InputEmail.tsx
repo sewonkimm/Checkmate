e44f09db069d06d16f47a05c0971752ed62d60c6
hsy118 <hsy118@hotmail.co.kr>
2021-04-29 15:50:07 +0900

    feat: 🍎 이메일중복 확인 버튼 삭제, 조건부 스타일링 추가
    ✅ Closes: #S04P31A106-232

--- frontend/src/Register/components/InputEmail.tsx
+++ frontend/src/Register/components/InputEmail.tsx
@@ -11,6 +11,7 @@ const InputEmail: React.FC<Props> = (props: Props) => {
   const [emailValue, setEmailValue] = useState<string>('');
   const [isValidEmail, setIsValidEmail] = useState<boolean>(false);
   const [dupleMsg, setDupleMsg] = useState<string>('');
+  const [isDuple, setIsDuple] = useState<boolean>(false);
 
   // Email 유효성 판단 함수
   const isEmail = (email: string) => {
@@ -21,17 +22,13 @@ const InputEmail: React.FC<Props> = (props: Props) => {
   // 유효성 검사 함수
   
   const checkValidEmail = debounce(async (value: string) => {
-
     if (isValidEmail) {
-        // setIsValidEmail(true);
-        onSubmitForm(value);
-      }
-      //  else {
-      //   setIsValidEmail(false);
-      // }
-  }, 300);
+      onSubmitForm(value);
+    }
+
+  }, 400);
   
-  // let timer: ReturnType<typeof setTimeout>;
+
 
   // input event 핸들러
   const onChangeInput = async (e:  React.ChangeEvent<HTMLInputElement>) => {
@@ -50,11 +47,12 @@ const InputEmail: React.FC<Props> = (props: Props) => {
       console.log('api 호출');
       const response = await validateEmailAPI(`/members/email/${email}`);
       if (response === 0) {
-        setDupleMsg('이메일 노중복');
-        // console.log(`이메일 노중복!!${email}`)
+        setDupleMsg('사용가능한 이메일입니다 :)');
+        setIsDuple(true);
         props.putEmail(email); // 중복이 안되면 부모 컴포넌트에 email 전달
       } else {
-        setDupleMsg('이메일 중복');
+        setDupleMsg('중복하는 이메일입니다 :(');
+        setIsDuple(false);
       }
     }
 
@@ -70,14 +68,17 @@ const InputEmail: React.FC<Props> = (props: Props) => {
         )}
       </Warning>
       <EmailInputWrapper>
-        <EmailInput
+        <EmailInput checkDuple={isDuple}
           value={emailValue}
           onChange={onChangeInput}
           type="text"
           placeholder="ssafy123@ssafy.com"
         />
       </EmailInputWrapper>
-      {dupleMsg && <h3>{dupleMsg}</h3>}
+      {dupleMsg && 
+      <DupleMsgWrapper isDuple={isDuple}>
+        {dupleMsg}
+      </DupleMsgWrapper>}
     </>
   );
 };
@@ -88,12 +89,13 @@ const Warning = styled.h3``;
 const EmailInputWrapper = styled.div`
   width: 473px;
 `;
-const EmailInput = styled.input`
+const EmailInput = styled.input<{ checkDuple : boolean }>`
   width: 100%;
   height: 72px;
+  border: 3px solid ${(props) => (props.checkDuple ? '#038EFC' : '#F016DE')};
   border-radius: 8px;
   padding: 22px 25px 23px 25px;
-  color: #038EFC;
+  color: ${(props) => (props.checkDuple ? '#038EFC' : '#F016DE')};
   font-size: 18px;
   &::placeholder {
     color: #038EFC;
@@ -101,6 +103,12 @@ const EmailInput = styled.input`
     font-weight: 400;
     font-size: 18px;
   }
+  &:focus {
+    outline: none;
+  }
+`;
+const DupleMsgWrapper = styled.h3<{isDuple: boolean}>`
+  color: ${(props) => (props.isDuple? '#FFFFFF' : '#F600E1')};
 `;
 
 export default InputEmail;
