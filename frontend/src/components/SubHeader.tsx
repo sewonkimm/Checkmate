bc854b2addc6f2d84a75c00a6eb52765f9dd98eb
김세원 <swon962@gmail.com>
2021-04-30 07:00:31 +0000

    #S04P31A106-187 로그인 여부에 따라 sub header 변경

--- frontend/src/components/SubHeader.tsx
+++ frontend/src/components/SubHeader.tsx
@@ -1,13 +1,49 @@
-import React, { ReactElement } from 'react';
-import { Link } from 'react-router-dom';
+import React, { ReactElement, useState, useEffect } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 import styled from 'styled-components';
+import { RootState } from '../modules';
+import { MemberType } from '../entity';
+import { logout } from '../modules/member';
 
 const SubHeader = (): ReactElement => {
+  const [member, setMember] = useState<MemberType | null>(useSelector((state: RootState) => state.member.member));
+  const [isMember, setIsMember] = useState<boolean>(false);
+  const [profileLink, setProfileLink] = useState<string>('');
+  const dispatch = useDispatch();
+  const router = useHistory();
+
+  useEffect(() => {
+    if (member === null) {
+      setIsMember(false);
+    } else if (member.memberId > 0) {
+      setIsMember(true);
+      setProfileLink(`/profile/${member.memberId}`);
+    }
+  }, [member]);
+
+  // 로그아웃 액션 호출
+  const onClickLogoutBtn = () => {
+    dispatch(logout());
+    setMember(null);
+    router.push('/');
+  };
+
   return (
     <SubHeaderContainer>
       {/* 언어 선택 버튼 추가 */}
-      <StyledLink to="/login">로그인</StyledLink>
-      <StyledLink to="/register">회원가입</StyledLink>
+
+      {isMember ? (
+        <>
+          <StyledLink to={profileLink}>마이페이지</StyledLink>
+          <LogoutBtn onClick={onClickLogoutBtn}>로그아웃</LogoutBtn>
+        </>
+      ) : (
+        <>
+          <StyledLink to="/login">로그인</StyledLink>
+          <StyledLink to="/register">회원가입</StyledLink>
+        </>
+      )}
     </SubHeaderContainer>
   );
 };
@@ -24,6 +60,15 @@ const SubHeaderContainer = styled.div`
 `;
 
 const StyledLink = styled(Link)`
+  text-decoration: none;
+  margin-left: 30px;
+  color: ${({ theme }) => theme.colors.white};
+  font-size: ${({ theme }) => theme.fontSizes.body};
+  font-weight: normal;
+  cursor: pointer;
+`;
+
+const LogoutBtn = styled.a`
   text-decoration: none;
   margin-left: 30px;
   color: ${({ theme }) => theme.colors.white};
