7c9437ee64db675def0637bd1e27baf14ca8122e
sewonkimm <swon962@gmail.com>
2021-04-30 15:18:56 +0900

    feat: 🍎 로그인시 sub header 내용 변경
    ✅ Closes: #S04P31A106-187

--- frontend/src/components/SubHeader.tsx
+++ frontend/src/components/SubHeader.tsx
@@ -1,13 +1,42 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import styled from 'styled-components';
+import { RootState } from '../modules';
+import { MemberType } from '../entity';
 
 const SubHeader = (): ReactElement => {
+  const [member, setMember] = useState<MemberType>(useSelector((state: RootState) => state.member.member));
+  const [isMember, setIsMember] = useState<boolean>(false);
+  const [profileLink, setProfileLink] = useState<string>('');
+
+  useEffect(() => {
+    if (member.memberId > 0) {
+      setIsMember(true);
+      setProfileLink(`/profile/${member.memberId}`);
+    }
+  }, [member]);
+
+  // 로그아웃 액션 호출
+  const onClickLogoutBtn = () => {
+    console.log('logout');
+  };
+
   return (
     <SubHeaderContainer>
       {/* 언어 선택 버튼 추가 */}
-      <StyledLink to="/login">로그인</StyledLink>
-      <StyledLink to="/register">회원가입</StyledLink>
+
+      {isMember ? (
+        <>
+          <StyledLink to={profileLink}>마이페이지</StyledLink>
+          <LogoutBtn onClick={onClickLogoutBtn}>로그아웃</LogoutBtn>
+        </>
+      ) : (
+        <>
+          <StyledLink to="/login">로그인</StyledLink>
+          <StyledLink to="/register">회원가입</StyledLink>
+        </>
+      )}
     </SubHeaderContainer>
   );
 };
@@ -24,6 +53,15 @@ const SubHeaderContainer = styled.div`
 `;
 
 const StyledLink = styled(Link)`
+  text-decoration: none;
+  margin-left: 30px;
+  color: ${({ theme }) => theme.colors.white};
+  font-size: ${({ theme }) => theme.fontSizes.body};
+  font-weight: normal;
+  cursor: pointer;
+`;
+
+const LogoutBtn = styled.a`
   text-decoration: none;
   margin-left: 30px;
   color: ${({ theme }) => theme.colors.white};
