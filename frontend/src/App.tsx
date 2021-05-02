53884dca247032b4b3e528a51acf958ede11c668
sewonkimm <swon962@gmail.com>
2021-05-02 13:29:44 +0900

    feat: 🍎 질문 작성 페이지 라우터 추가
    ✅ Closes: #S04P31A106-155

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,6 +11,7 @@ import theme from './styles/theme';
 import Home from './Home/Index';
 import Register from './Register/Index';
 import Login from './Login/Index';
+import QuestionWrite from './QuestionWrite';
 
 const App = (): ReactElement => {
   return (
@@ -21,6 +22,7 @@ const App = (): ReactElement => {
         <Route exact path="/" component={Home} />
         <Route path="/register" component={Register} />
         <Route path="/login" component={Login} />
+        <Route path="/question/write" component={QuestionWrite} />
       </Switch>
     </ThemeProvider>
   );
---
+++ frontend/src/QuestionWrite/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const QuestionWrite: React.FC = () => {
+  return <div>Question wirte</div>;
+};
+
+export default QuestionWrite;
