64226112ff3297226b5947bcfe7aa0209edfac60
ingorithm <ingu94@gmail.com>
2021-05-06 16:04:24 +0900

    feat: 🍎 #S04P31A106-261 맞춤법 검사 POST 구현
    ✅ Closes: #S04P31A106-261

---
+++ AI/app/routers/checkSpell.py
@@ -0,0 +1,47 @@
+from fastapi import APIRouter
+from pydantic import BaseModel
+from hanspell import spell_checker
+from hanspell.constants import CheckResult
+
+router = APIRouter()
+
+class Data(BaseModel):
+    sentence: str
+
+@router.post('/checkSpell')
+def checkSpell(Sentences: Data):
+    sentence = Sentences.sentence.replace('\n', '')
+    checked_sent = spell_checker.check(sentence)
+
+    # 띄어쓰기가 없는 문장 임의로 만들기
+    unspaced_sent = sentence.replace(' ', '')
+    spaced_checked_sent = spell_checker.check(unspaced_sent)
+
+    total = 0
+    errors = 0
+    wrong_spelling = 0
+    wrong_spacing = 0
+    ambiguous = 0
+    statistical_correction = 0
+
+    for key, value in checked_sent.words.items():
+
+        total = total + 1
+
+        if value == CheckResult.WRONG_SPELLING:
+            wrong_spelling = wrong_spelling + 1
+            errors = errors + 1
+        elif value == CheckResult.WRONG_SPACING:
+            wrong_spacing = wrong_spacing + 1
+            errors = errors + 1
+        elif value == CheckResult.AMBIGUOUS:
+            ambiguous = ambiguous + 1
+            errors = errors + 1
+        elif value == CheckResult.STATISTICAL_CORRECTION:
+            statistical_correction = statistical_correction + 1
+            errors = errors + 1
+
+    return {"original": checked_sent.original, "checked": spaced_checked_sent.checked,
+            "errors": errors, "error_rate": round(errors / total * 100, 2),
+            "wrong_spelling": wrong_spelling, "wrong_spacing": wrong_spacing,
+            "ambiguous": ambiguous, "statistical_correction": statistical_correction}
---
+++ AI/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+import uvicorn
+from app.routers import checkSpell
+
+app = FastAPI()
+app.include_router(checkSpell.router)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
