64f8b400dc725f18a91c03b3046ea3950f9748e8
hsy118 <hsy118@hotmail.co.kr>
2021-05-10 19:40:22 +0900

    feat: 🍎 마이페이지 받은 리뷰 조회
    ✅ Closes: #S04P31A106-329

--- frontend/src/pages/MyPage/components/MyReview.tsx
+++ frontend/src/pages/MyPage/components/MyReview.tsx
@@ -32,7 +32,7 @@ const MyReview = (props: PropsType): ReactElement => {
             );
           })
         ) : (
-          <span>받으신 리뷰가 없습니다</span>
+          <span>받으신 리뷰가 없습니다 ㅠㅠ</span>
         )}
       </ReviewCards>
       <GetMoreReviewBtn>후기 더 보기</GetMoreReviewBtn>
