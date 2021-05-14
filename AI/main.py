f44de3ff830934a423bd62ad746e32e832e94894
sewonkimm <swon962@gmail.com>
2021-05-14 10:55:35 +0900

    fix: 🐛 python 관련 코드 원상복구

--- AI/main.py
+++ AI/main.py
@@ -1,23 +1,30 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
-from app.routers import checkSpell
+from dataclasses import asdict
+from starlette.middleware.cors import CORSMiddleware
+from app.common.config import conf
+from app.database.conn import db
+from app.routers import checkSpell, recommendWords
 
+c = conf()
 app = FastAPI()
-app.include_router(checkSpell.router)
 
-origins = [
-    "http://localhost",
-    "http://localhost:3000",
-]
+# DataBase Initialization
+conf_dict = asdict(c)
+db.init_app(app, **conf_dict)
 
+# Middleware Definition
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=conf().ALLOW_SITE,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+# Router Definition
+app.include_router(checkSpell.router)
+app.include_router(recommendWords.router)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
