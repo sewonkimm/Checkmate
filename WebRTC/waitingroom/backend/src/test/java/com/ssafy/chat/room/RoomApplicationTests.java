7770e0879bfbc5a69aa6d02aaed7dcb817089a62
rudus1012 <rudus1012@naver.com>
2021-05-17 17:33:43 +0900

    feat: 🍎 waitingroom project
    ✅ Closes: #S04P31A106-434

---
+++ WebRTC/waitingroom/backend/src/test/java/com/ssafy/chat/room/RoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.chat.room;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
